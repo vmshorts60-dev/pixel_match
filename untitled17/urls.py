242911faaa378e02929b85351625320702ceea2c
vmshorts60-dev <vmshorts60@gmail.com>
2026-06-10 00:27:48 +0530

    Fix root URL

--- untitled17/urls.py
+++ untitled17/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pixel_match/', include('pixel_match.urls'))
+    path('', include('pixel_match.urls'))
 ]
 
 if settings.DEBUG:
